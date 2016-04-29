8594acb5037869f8d35b60e3bc51759145e58657
matthew-shaw <matthew.shaw@digital.landregistry.gov.uk>
2016-04-29 14:21:33 +0100

    pg 9.5 pgis 2.2

--- provision.sh
+++ provision.sh
@@ -1,12 +1,11 @@
-echo 'Updating package lists...'
-apt-get -qq  update
-
 # Install PostGIS
 echo *** Installing PostGIS ***
-	sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt trusty-pgdg main" >> /etc/apt/sources.list'
-	wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | apt-key add -
-	sudo apt-add-repository -y ppa:georepublic/pgrouting
-	sudo apt-get install -y postgresql-9.4-postgis-2.1 pgadmin3 postgresql-contrib
+	sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+	sudo apt-get install wget ca-certificates
+	wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+	sudo apt-get update
+	sudo apt-get -y upgrade
+	sudo apt-get install -y postgresql-9.5-postgis-2.2 pgadmin3 postgresql-contrib
 # Enable Adminpack
 #	sudo -u postgres psql
 #	CREATE EXTENSION adminpack;
